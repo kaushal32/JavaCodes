c7e6a1a2c0c814369b6c1d87d3566ca617bf3faa
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-11-19 15:05:54 +0530

    To return copy of 1st 2 chars of a string n times

--- src/Chars.java
+++ src/Chars.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
-public class Repeat {
+public class Chars {
     String s;
-    int n;
-    Repeat(){
+    Chars(){
         Scanner ss = new Scanner(System.in);
         s= ss.next();
-        n=ss.nextInt();
     }
 
     public static void main(String[] args) {
-        Repeat obj = new Repeat();
+        Chars obj = new Chars();
         int ls1 = obj.s.length();
-        String s1 = obj.s.substring(ls1-obj.n,ls1);
-        String s2 ="";
-        for(int i=0;i<obj.n;i++){
-            s2+= s1;
+        String s1 = obj.s.substring(0,2);
+        String s2="";
+        for (int i=0; i<ls1;i++){
+            s2 = s2+s1;
         }
         System.out.println(s2);
     }
-}
