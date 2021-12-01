cc5c8b139861ee39110a8dd369bae772ad530c9a
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-12-01 19:20:57 +0530

    Basic Ques.

---
+++ src/ElectricityLoad.java
@@ -0,0 +1,19 @@
+public class ElectricityLoad
+{
+    boolean AC_ON;
+    boolean HOME_THEATRE_ON;
+    boolean FAN_ON;
+    boolean LIGHT_ON;
+
+//    ac = 1200;
+//    home theatre = 600;
+//    light = 100;
+//    fan = 400;
+}
+class ElectrcityLoadMain
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
